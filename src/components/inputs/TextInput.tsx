1cb762b0dd223c02549c926695e4158c7af6468f
edersonfaccin <edersonfrasson@gmail.com>
2022-11-22 13:11:23 -0300

    feat: changes

--- src/components/inputs/TextInput.tsx
+++ src/components/inputs/TextInput.tsx
@@ -6,24 +6,23 @@ const { Text } = Typography;
 
 const TextInput = (props: IInput) => {
 
-    return (
-      <>
-        <Text>{props.label}</Text>
-        <Form.Item
-          style={{
-            flexDirection: 'column',
-            paddingRight: 2
-          }}
-          //label={props.label}
-          colon={false}
-          name={props.property}
-          rules={props.rules}>
-          <Input style={{
-            borderRadius: 10
-          }}/>
-        </Form.Item>
-      </>
-    )
+  return (
+    <>
+      <Text>{props.label}</Text>
+      <Form.Item
+        style={{
+          flexDirection: 'column',
+          paddingRight: 2
+        }}
+        colon={false}
+        name={props.property}
+        rules={props.rules}>
+        <Input style={{
+          borderRadius: 10
+        }}/>
+      </Form.Item>
+    </>
+  )
 };
 
 export default TextInput;
